e78f4a96524865595277861ce41a5942c522f4b6
avaughn01 <avaughn01@unomaha.edu>
2017-03-07 16:41:59 -0600

    Add files via upload

---
+++ PuzzlePiece.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Brian Ricks, PhD, 2016. bricks at unomaha.edu
+ */
+
+package project;
+
+/**
+/* The class PuzzlePiece
+*/
+public class PuzzlePiece
+{
+    
+    public int x = 100;
+    public int y = 100;
+    
+    /**
+    /* Default constructor
+    */
+    public PuzzlePiece()
+    {
+
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}
